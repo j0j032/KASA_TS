505e36e6d780a85cfac160b4c0a363e579a36338
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-20 13:51:22 +0200

    ✨ got a map

--- src/components/Map/MainMap.tsx
+++ src/components/Map/MainMap.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Map from 'react-map-gl'
 
 const MainMap: React.FC = (): JSX.Element => {
@@ -8,12 +8,12 @@ const MainMap: React.FC = (): JSX.Element => {
             <Map
                 mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
                 initialViewState={{
-                    longitude: 2.320041,
+                    longitude: 2.335,
                     latitude: 48.8588897,
                     zoom: 11
                 }}
                 style={{width: '100%', height: '100%'}}
-                mapStyle='mapbox://styles/mapbox/streets-v9'
+                mapStyle='mapbox://styles/j0j032/cl9gyr4ep008x15pu513jnsu4/draft'
             >
                 Markers here</Map>
         </section>
