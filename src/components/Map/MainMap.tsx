b2db68a57a86e2e2167f669194f79abafa8516f4
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-20 15:22:13 +0200

    ✨ get map pins

--- src/components/Map/MainMap.tsx
+++ src/components/Map/MainMap.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Map, {Marker} from 'react-map-gl'
 
-const MainMap: React.FC = (): JSX.Element => {
-
+const MainMap: React.FC = ({lodgings}): JSX.Element => {
+    const {adress, coordinates, id} = lodgings
+    console.log(lodgings)
     return (
         <section className='map__container'>
             <Map
@@ -15,7 +16,11 @@ const MainMap: React.FC = (): JSX.Element => {
                 style={{width: '100%', height: '100%'}}
                 mapStyle='mapbox://styles/j0j032/cl9gyr4ep008x15pu513jnsu4/draft'
             >
-                
+                {lodgings.map((lodging) => (
+                    <Marker key={lodging.id} longitude={lodging.coordinates[1]} latitude={lodging.coordinates[0]}>
+                        <div className='map__pin'>⌂</div>
+                    </Marker>
+                ))}
 
             </Map>
         </section>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,18 +3,38 @@ import Header from "../../components/Header/Header";
 import Filters from '../../components/Filters/Filters'
 import Gallery from "../../components/Gallery/Gallery";
 import MainMap from "../../components/Map/MainMap";
+import {useQuery} from "react-query";
+import {lodging} from "../../types";
+import {AxiosError} from "axios";
+import {getLodgings} from "../../api/lodging.requests";
 
 const Home = () => {
+
+    const {
+        data,
+        isLoading,
+        isError,
+        error
+    } = useQuery<Array<lodging>, AxiosError>(['lodgings'], () => getLodgings, {
+        staleTime: 100_000,
+        retry: false
+    })
+
+
+    if (isError) return <p>Error: {error.message}</p>
+    if (isLoading) return <p>LOADING ...</p>
+
+
     return (
         <>
             <Header/>
             <Filters/>
             <main className='divide-content'>
                 <Gallery/>
-                <MainMap/>
+                <MainMap lodgings={data}/>
             </main>
         </>
     );
 };
 
-export default Home;
+export default Home
