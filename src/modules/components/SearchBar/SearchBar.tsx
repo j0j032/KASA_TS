391d51ccfc8376676b2e99b13a04a028e29e4ae6
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-30 17:49:38 +0100

    🚧 implementing search with submit func

--- src/modules/components/SearchBar/SearchBar.tsx
+++ src/modules/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,21 @@
-import React, {memo, useState} from 'react';
+import React, {memo, useRef, useState} from 'react';
 import {FaSearch} from "react-icons/fa";
-import {useGetLodgings} from "../../../api/useGetLodgings";
 import {useQueryClient} from "react-query";
 
 
 const SearchBar = ({setFilter}) => {
+    const search = useRef()
 
+    const handleChange = (e) => {
+        e.preventDefault()
+        setFilter(search.current.value)
+    }
     return (
-        <form className='searchBar'>
+        <form onSubmit={handleChange} className='searchBar'>
 
-            <input className='searchBar__input'
+            <input ref={search} className='searchBar__input'
                    type='text'
                    placeholder='Recherche ...'
-                   onChange={(e) => setFilter(e.target.value)}
             />
             <button className='searchBar__btn'><FaSearch className='searchBar__icon'/></button>
         </form>
